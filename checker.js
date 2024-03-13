0ca46e3a47ff30618a3b23c72c161427f1d5c7ef
Raraph84 <contact@raraph.fr>
2024-03-13 21:25:10 +0100

    First commit

---
+++ checker.js
@@ -0,0 +1,119 @@
+const { request: httpsRequest } = require("https");
+const { request: httpRequest } = require("http");
+const { pingWithPromise } = require("minecraft-ping-js");
+const Ws = require("ws");
+
+const checkWebsite = (host) => new Promise((resolve, reject) => {
+
+    let finishDate = 0;
+    let responseDate = 0;
+
+    const req = (host.startsWith("https") ? httpsRequest : httpRequest)(host, { agent: false });
+    req.on("finish", () => finishDate = Date.now());
+    req.on("response", (res) => {
+        responseDate = Date.now();
+        res.on("data", () => { });
+        res.on("end", () => {
+
+            if (res.statusCode !== 200) {
+                reject("Status code (" + res.statusCode + ") is not 200");
+                return;
+            }
+
+            resolve(responseDate - finishDate);
+        });
+    });
+    req.on("error", (error) => reject(error));
+    req.end();
+
+    setTimeout(() => reject(new Error("timeout")), 10000);
+});
+
+const checkApi = (host) => new Promise((resolve, reject) => {
+
+    let finishDate = 0;
+    let responseDate = 0;
+
+    const req = (host.startsWith("https") ? httpsRequest : httpRequest)(host, { agent: false });
+    req.on("finish", () => finishDate = Date.now());
+    req.on("response", (res) => {
+        responseDate = Date.now();
+        let data = "";
+        res.on("data", (chunk) => data += chunk);
+        res.on("end", () => {
+
+            try {
+                JSON.parse(data);
+            } catch (error) {
+                reject("Invalid JSON");
+                return;
+            }
+
+            resolve(responseDate - finishDate);
+        });
+    });
+    req.on("error", (error) => reject(error));
+    req.end();
+
+    setTimeout(() => reject(new Error("timeout")), 10000);
+});
+
+const checkWs = (host) => new Promise((resolve, reject) => {
+
+    let openTime = -1;
+
+    const ws = new Ws(host);
+
+    ws.on("open", () => {
+        openTime = Date.now();
+        ws.send("");
+    });
+
+    ws.on("close", () => {
+        if (openTime > 0) resolve(Date.now() - openTime);
+    });
+
+    ws.on("error", (error) => {
+        reject(error);
+    });
+
+    setTimeout(() => reject(new Error("timeout")), 10000);
+});
+
+const checkBot = (host) => new Promise((resolve, reject) => {
+
+    fetch(host).then((res) => {
+
+        if (res.status !== 200) {
+            reject("Check failed");
+            return;
+        }
+
+        res.json().then((json) => {
+
+            if (json.online) resolve();
+            else reject("Bot offline");
+
+        }).catch(() => reject("Check failed"));
+
+    }).catch(() => reject("Check failed"));
+
+    setTimeout(() => reject("Check failed"), 10000);
+});
+
+const checkMinecraft = (host) => new Promise((resolve, reject) => {
+
+    pingWithPromise(host.split(":")[0], parseInt(host.split(":")[1]) || 25565)
+        .then((res) => resolve(res.ping))
+        .catch((error) => reject(error));
+
+    // Timeout of 10s is handled by the library
+});
+
+module.exports = {
+    checkWebsite,
+    checkApi,
+    checkWs,
+    checkBot,
+    checkMinecraft
+}
---
+++ index.js
@@ -0,0 +1,105 @@
+const { getConfig, HttpServer } = require("raraph84-lib");
+const pLimit = require("p-limit");
+const { checkWebsite, checkMinecraft, checkApi, checkWs, checkBot } = require("./checker");
+const config = getConfig(__dirname);
+
+const server = new HttpServer();
+server.on("request", async (/** @type {import("raraph84-lib/src/Request")} */ request) => {
+
+    if (request.url !== "/check") {
+        request.end(404, "Not found");
+        return;
+    }
+
+    if (request.method !== "POST") {
+        request.end(405, "Method not allowed");
+        return;
+    }
+
+    if (!request.headers.authorization) {
+        request.end(401, "Missing authorization");
+        return;
+    }
+
+    if (request.headers.authorization !== config.token) {
+        request.end(401, "Invalid token");
+        return;
+    }
+
+    if (!request.jsonBody) {
+        request.end(400, "Invalid JSON");
+        return;
+    }
+
+    if (typeof request.jsonBody.checks === "undefined") {
+        request.end(400, "Missing checks");
+        return;
+    }
+
+    if (!Array.isArray(request.jsonBody.checks)) {
+        request.end(400, "Checks must be an array");
+        return;
+    }
+
+    for (const check of request.jsonBody.checks) {
+
+        if (typeof check !== "object") {
+            request.end(400, "Check must be an object");
+            return;
+        }
+
+        if (typeof check.type === "undefined") {
+            request.end(400, "Missing type");
+            return;
+        }
+
+        if (typeof check.type !== "string") {
+            request.end(400, "Type must be a string");
+            return;
+        }
+
+        if (!["website", "minecraft", "api", "gateway", "bot"].includes(check.type)) {
+            request.end(400, "Invalid type");
+            return;
+        }
+
+        if (typeof check.host === "undefined") {
+            request.end(400, "Missing host");
+            return;
+        }
+
+        if (typeof check.host !== "string") {
+            request.end(400, "Host must be a string");
+            return;
+        }
+    }
+
+    const limit = pLimit(4);
+
+    const checks = await Promise.all(request.jsonBody.checks.map((check) => limit(async () => {
+
+        let responseTime;
+        try {
+            if (check.type === "website") responseTime = await checkWebsite(check.host);
+            else if (check.type === "minecraft") responseTime = await checkMinecraft(check.host);
+            else if (check.type === "api") responseTime = await checkApi(check.host);
+            else if (check.type === "gateway") responseTime = await checkWs(check.host);
+            else if (check.type === "bot") await checkBot(check.host);
+        } catch (error) {
+            return { online: false, error: error.toString() };
+        }
+
+        return { online: true, responseTime };
+
+    })));
+
+    request.end(200, { checks });
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+console.log("Lancement du serveur...");
+server.listen(port).then(() => {
+    console.log("Serveur lancé sur le port " + port + " !");
+}).catch((error) => {
+    console.log("Impossible de lancer serveur ! " + error);
+});
